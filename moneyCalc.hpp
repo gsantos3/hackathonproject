275112b65b5bfa5362f94016e03bf715e4102553
Gian Santos <gsantos08@csu.fullerton.edu>
2021-09-05 21:52:08 +0000

    added class

--- moneyCalc.hpp
+++ moneyCalc.hpp
@@ -1,1 +1,27 @@
+#ifdef __MONEYCALC_HPP__
+#define __MONEYCALC_HPP__
+#include <string>
 
+const int MAX_PRODUCTS = 3
+
+class Prodcut 
+  {
+    private:
+    std::string Name;
+    double Price;
+    unsigned int Rating;
+    long budget;
+    
+    public:
+    void setName(std::string n) {Name = n;}
+    void setPrice (double p) {Price = p;}
+    void setRating (unsigned int r) {Rating = r;}
+
+    std::string getName() {return Name;}
+    double setPrice() {return Price;}
+    unsigned int setRating {return Rating;}
+  };
+
+  Product lowestPrice(Product prod[], int size );
+
+  #endif
